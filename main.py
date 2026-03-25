e6d53a88dc1ba6592bce8d6068ad3b55d656a88f
edwardtacas-netizen <edward.tacas@neo.com.pe>
2026-03-25 16:33:12 -0500

    clase02

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def saludar():
+    return {"mensaje": "¡Hola! Bienvenido a mi API"}
+@app.get("/bienvenido/{nombre}")
+def saludar_persona(nombre: str):
+    return {"mensaje": f"Hola {nombre}, ¡qué bueno verte por aquí!"}
+
+@app.get("/servicios")
+def listar_servicios():
+    return {
+        "servicios": servicios_db
+    }
+
+@app.post("/agregar-servicio")
+def agregar_servicio(nuevo: dict):
+    servicios_db.append(nuevo)
+    return {
+        "mensaje": "¡Servicio guardado!"
+    }
+
+
+servicios_db = [
+    {"nombre": "consulta", "precio": 50},
+    {"nombre": "baño", "precio": 60},
+    {"nombre": "corte", "precio": 100}
+]
